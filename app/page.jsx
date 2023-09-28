61d24912daf9960f40f5f283b356f9bedb503389
Sertaç Can <sertac.can1@gmail.com>
2023-09-28 16:33:20 +0300

    update loading state

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,12 @@ import LastSection from "@/components/last-section";
 import Manage from "@/components/manage-section";
 import Pattern from "@/components/pattern-image";
 
-export default function Home() {
+async function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export default async function Home() {
+  await delay(2000);
   return (
     <>
       <Pattern />
